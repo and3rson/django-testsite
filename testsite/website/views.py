e0322bc5c3ffb2c60e03572fb5f5b08ca5424e13
and3rson <anderson.dunai@gmail.com>
2016-01-24 23:55:35 +0200

    Pages.

--- testsite/website/views.py
+++ testsite/website/views.py
@@ -7,4 +7,8 @@ def view_page(request, page_slug):
         page = get_object_or_404(Page, is_main_page=True)
     else:
         page = get_object_or_404(Page, slug=page_slug)
-    return render(request, 'templates/page.html', dict(page=page))
+    pages = Page.objects.order_by('name')
+    return render(request, 'templates/page.html', dict(
+        page=page,
+        pages=pages
+    ))
